e6ff65d120c95ecb529236aed0ddc80bc9ce413c
Nabate George <nabateg123@gmail.com>
2019-03-19 12:54:21 -0700

    added new files

---
+++ example.js
@@ -0,0 +1,12 @@
+// a function to show date
+function showdate() {
+    document.getElementById('date').innerHTML = Date();
+}
+
+function add() {
+    var a=10;
+    var b=20;
+    // adds two numbers
+    result = a+b;
+    document.getElementById('result').innerHTML = result;
+}
